18d753c4d5431fb7e6f04a7f00c1b43852a61e57
Drayas <andreasphoenix@gmail.com>
2016-01-13 15:07:37 -0800

    Basic functionality for dogs coming and going

--- app/models/yard_state.js
+++ app/models/yard_state.js
@@ -2,6 +2,7 @@
 
 var React = require('react-native');
 var _     = require('lodash-node');
+var dogs  = require("../tables/dogs");
 var {
 } = React;
 
@@ -11,11 +12,11 @@ var YardState = {
 		lastUpdate: 1451856564213,
 		slots: {
 			1: { toyId: 1},
-			2: { dogId: 2, toyId: 2, dogWillLeave: 1451856564213},
-			3: { dogId: 3, toyId: 3, dogWillLeave: 1451856564213},
-			4: { dogId: 4, toyId: 4, dogWillLeave: 1451856564213},
-			5: { dogId: 5, toyId: 5, dogWillLeave: 1451856564213},
-			6: { dogId: 1, toyId: 6, dogWillLeave: 1451856564213},
+			2: { toyId: 2},
+			3: { toyId: 3},
+			4: { toyId: 4},
+			5: { toyId: 5},
+			6: { toyId: 6},
 		}
 
 	},
@@ -28,18 +29,58 @@ var YardState = {
         console.log( "removing dog " + slotId );
 				self._removeDog(slotId);
       }
-		});	
+		});
 	},
 
 	_removeDog(slotId) {
-		delete this._state.slots[slotId].dogId;
+		var dogId = this._state.slots[slotId].dogId;
+		var timeLeft = this._state.slots[slotId].dogWillLeave;
+    delete this._state.slots[slotId].dogId;
 		delete this._state.slots[slotId].dogWillLeave;
+    delete dogs[dogId].inSlot;
+    dogs[dogId].lastTimeLeft = timeLeft;
 	},
 
-	updateState() {
+  _addDogs() {
+    var self = this;
+    var slotsSeekingDogs = {};
+		_.each(this.getState().slots, function(slot, slotId) {
+			if (!slot.dogId && slotId != 1) { //no Tubbs
+        if(Math.random() <= 0.75) { //75% chance
+          slotsSeekingDogs[slotId] = slot;
+        }
+      }
+		});
+		_.each(slotsSeekingDogs, function(slot, slotId) {
+        console.log( "find dog for slot " + slotId );
+				self._addDog(slotId);
+    });
+	},
+
+	_addDog(slotId) {
+    var self = this;
+    var curTime = (new Date).getTime();
+    var dogFoundId = 0;
+    var dogFoundPower = 0;
+ 		_.each(dogs, function(dog, dogId) {
+      if (!dog.inSlot && dog.lastTimeLeft == 0) { //sleepTime not implemented yet
+        if(dog.power > dogFoundPower) {
+          dogFoundId = dogId;
+        }
+      }
+		});
+        console.log( "adding dog " + dogs[dogFoundId].name +
+                    " slot " + slotId);
+        self.getState().slots[slotId].dogId = dogFoundId;
+        self.getState().slots[slotId].dogWillLeave = curTime + 500000; //Playtime
+        dogs[dogFoundId].inSlot = slotId;
+	},
+
+  updateState() {
 		this.setState({lastUpdate: (new Date).getTime()});
     console.log( "updateState" );
 		this._removeDogs();
+    this._addDogs();
 		return this._state;
 	},
 
